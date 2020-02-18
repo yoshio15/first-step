6e78ed408ccb32cf0907e168dac55faa6130db76
YoshioKondo <yoshio15310@gmail.com>
2020-02-18 22:55:46 +0900

    会員登録完了画面レイアウト修正、アプリ全体背景色追加

--- src/components/pages/SignUpDone.tsx
+++ src/components/pages/SignUpDone.tsx
@@ -9,18 +9,44 @@ const SignUpDone: React.FC<Props> = (props) => {
   console.log(props)
   return (
     <Container>
+      <Box mt={8}></Box>
       <Paper>
         <Grid container justify='center'>
           <Grid item>
-            <Typography variant='h5'>
-              会員登録が完了しました
-            </Typography>
-            <Box mt={5}></Box>
-            <Button
-              color='primary'
-              variant='contained'
-              onClick={() => props.history.push('/login')}
-            >ログイン画面へ >></Button>
+            <Grid container justify='center'>
+              <Grid item>
+                <Box mt={5}></Box>
+                <Typography variant='h5'>
+                  アカウントの登録を受け付けました。
+                </Typography>
+              </Grid>
+            </Grid>
+            <Grid container justify='center'>
+              <Grid item>
+                <Box mt={5}></Box>
+                <Typography variant='h6'>
+                  ご登録いただいた「xxxxx@yyyy.zzz」宛に登録確認用のご案内をお送りしましたので、
+                </Typography>
+              </Grid>
+            </Grid>
+            <Grid container justify='center'>
+              <Grid item>
+                <Typography variant='h6'>
+                  メールの内容を確認して、First-Stepアカウントの登録作業を完了してください。
+                </Typography>
+              </Grid>
+            </Grid>
+            <Grid container justify='center'>
+              <Grid item>
+                <Box mt={5}></Box>
+                <Button
+                  color='primary'
+                  variant='contained'
+                  onClick={() => props.history.push('/login')}
+                >ログイン画面へ >></Button>
+                <Box mb={3}></Box>
+              </Grid>
+            </Grid>
           </Grid>
         </Grid>
       </Paper>
