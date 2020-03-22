1b0cbed551d8ddc28e92e8b134830bec5feecfd5
YoshioKondo <yoshio15310@gmail.com>
2020-03-22 16:13:30 +0900

    リスト取得API実装

--- src/components/pages/WorksList.tsx
+++ src/components/pages/WorksList.tsx
@@ -1,27 +1,72 @@
 import React from 'react'
 import { Container, Paper, Grid, Box } from '@material-ui/core'
+import { API } from 'aws-amplify'
 import Store from '../../store/index'
 
+interface ResponseListI {
+  workId: string,
+  title: string,
+  description: string,
+  userId: string,
+  userName: string
+}
+interface ResponseI {
+  work_id: string,
+  title: string,
+  description: string,
+  user_id: string,
+  user_name: string
+}
+interface StateI {
+  itemList: ResponseListI[]
+}
 class WorksList extends React.Component {
-  getItemList() {
-    return [
-      { id: '01', name: 'taro', msg: 'HelloWorld' },
-      { id: '02', name: 'jiro', msg: 'HelloWorld' },
-      { id: '03', name: 'saburo', msg: 'HelloWorld' },
-      { id: '04', name: 'yonro', msg: 'HelloWorld' },
-      { id: '05', name: 'goro', msg: 'HelloWorld' }
-    ]
+  state: StateI = {
+    itemList: []
+  }
+  componentDidMount() {
+    this.getItemList()
+  }
+  async getItemList() {
+    const apiName = 'dev-first-step';
+    const path = '/get-works-list';
+    const option = {}
+    let responseList!: ResponseListI[];
+    await API.get(apiName, path, option)
+      .then(response => {
+        console.log(response)
+        responseList = this.formatResponse(response)
+        console.log(responseList)
+      }).catch(error => {
+        console.log(error)
+      });
+    this.setState({ itemList: responseList })
+  }
+  formatResponse(res: any): ResponseListI[] {
+    return res.map((item: ResponseI) => {
+      return {
+        workId: item.work_id,
+        title: item.title,
+        description: item.description,
+        userId: item.user_id,
+        userName: item.user_name
+      }
+    })
   }
   render() {
     console.log(Store.getState().store)
     return (
       <Container>
         <h2>みんなの作品一覧</h2>
-        {this.getItemList().map(item => (
+        {this.state.itemList.map(item => (
           <Grid container justify='center'>
-            <Grid xs={6} item>
-              <Paper key={item.id}>{item.name}</Paper>
-              <Box mb={2}></Box>
+            <Grid xs={11} item>
+              <Paper key={item.workId}>
+                <h3>{item.title}</h3>
+                <div>{item.description}</div>
+                <div>{item.userName}</div>
+              </Paper>
+              <Box mb={1}></Box>
             </Grid>
           </Grid>
         ))}
